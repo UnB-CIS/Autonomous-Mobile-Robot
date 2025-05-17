e5b0faf2f859ae2e7a9cdbae500f9790aa4fe314
Thiago Gomes da Silva Lourenço <thiago.gsl70@gmail.com>
2025-05-17 10:47:26 -0300

    primeiro script de object avoidance

--- coppeliasim/scripts/pid_go_to_goal.py
+++ coppeliasim/scripts/pid_go_to_goal.py
@@ -11,12 +11,7 @@ robot = sim.getObject("/PioneerP3DX")
 usensors = [(sim.getObject("/PioneerP3DX/ultrasonicSensor", {"index": i})) for i in range(0,16)]
 motorLeft=sim.getObject("/PioneerP3DX/leftMotor")
 motorRight=sim.getObject("/PioneerP3DX/rightMotor")
-noDetectionDist=0.5
-maxDetectionDist=0.2
-detect=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-braitenbergL=[-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-braitenbergR=[-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-v0=2
+v0=0
 
 
 
@@ -30,22 +25,9 @@ while sim.getSimulationState() != sim.simulation_stopped:
     # Print the position and orientation
     # print("Position:", pioneer_position)
     # print("Orientation:", pioneer_orientation)
-    for i in range(0,16):
-        res, dist, _, _, _ = sim.readProximitySensor(usensors[i])
-
-        if (res>0) and (dist<noDetectionDist):
-            if (dist<maxDetectionDist):
-                dist=maxDetectionDist
-            detect[i]=1-((dist-maxDetectionDist)/(noDetectionDist-maxDetectionDist))
-        else:
-            detect[i]=0
 
     vLeft=v0
     vRight=v0
-
-    for i in range(0,16):
-        vLeft=vLeft+braitenbergL[i]*detect[i]
-        vRight=vRight+braitenbergR[i]*detect[i]
 
     # Setting Velocities
     sim.setJointTargetVelocity(motorLeft,vLeft)
