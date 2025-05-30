a42011d5481f899b04101a151310848567dca360
Thiago Gomes da Silva Lourenço <thiago.gsl70@gmail.com>
2025-05-29 21:30:23 -0300

    PID controller issue resolved and little changes in the scene

--- coppeliasim/scripts/pid_go_to_goal.py
+++ coppeliasim/scripts/pid_go_to_goal.py
@@ -9,16 +9,15 @@ class PID:
         self.e_prev = 0
         self.e_acc = 0
 
-    def update(self, x_desired, x): 
-        current_e = x_desired - x
+    def control(self, e): 
 
-        self.e_acc += current_e
+        self.e_acc += e
 
-        P = self.kp*current_e
-        I = self.ki*self.e_acc
-        D = self.kd*(current_e - self.e_prev)
+        P = self.kp * e
+        I = self.ki * self.e_acc
+        D = self.kd * (e - self.e_prev)
 
-        self.e_prev = current_e
+        self.e_prev = e
         return P + I + D
 
 client = RemoteAPIClient()
@@ -32,14 +31,14 @@ robot = sim.getObject("/PioneerP3DX")
 usensors = [(sim.getObject("/PioneerP3DX/ultrasonicSensor", {"index": i})) for i in range(0,16)]
 motorLeft=sim.getObject("/PioneerP3DX/leftMotor")
 motorRight=sim.getObject("/PioneerP3DX/rightMotor")
-R = (195/2)/1000
-L = (381/2)/1000
+R = 0.19502
+L = 0.331
 
 # Controller Parameters
 v_max_linear = 0.5
-kp = 1              # Proportional coefficient
-ki = 0              # Integrative coefficient
-kd = 0.1              # Derivative coefficient
+kp = 1.1              # Proportional coefficient
+ki = 0.001              # Integrative coefficient
+kd = 0.001              # Derivative coefficient
 k_linear_vel_damping = 1.0 # Ganho para reduzir velocidade perto do alvo
 max_wheel_velocity = 5 # Ex: 5.0 rad/s
 
@@ -52,15 +51,15 @@ while sim.getSimulationState() != sim.simulation_stopped:
 
     x_desired, y_desired, _ = sim.getObjectPosition(target, -1)
     
-    print("Target position:", (x_desired, y_desired))
+    #print("Target position:", (x_desired, y_desired))
 
     # # Get the position and orientation of the Pioneer robot
     x, y, _ = sim.getObjectPosition(robot, -1)
-    phi, _, _ = sim.getObjectOrientation(robot, -1)
+    _, _, phi = sim.getObjectOrientation(robot, -1)
     
     # Print the position and orientation
-    # print("Position:", pioneer_position)
-    # print("Orientation:", pioneer_orientation)
+    # print("Position:", (x, y))
+    # print("Orientation:", phi)
 
     # Distância e ângulo para o alvo
     delta_x = x_desired - x
@@ -72,7 +71,7 @@ while sim.getSimulationState() != sim.simulation_stopped:
     angle_error = phi_desired - phi
     angle_error = np.arctan2(np.sin(angle_error), np.cos(angle_error))
 
-    omega = pid.update(0, -angle_error)
+    omega = pid.control(angle_error)
 
     v_current_linear = min(v_max_linear, k_linear_vel_damping * distance_to_target)
 
@@ -82,13 +81,8 @@ while sim.getSimulationState() != sim.simulation_stopped:
     vLeft = np.clip(vLeft, -max_wheel_velocity, max_wheel_velocity)
     vRight = np.clip(vRight, -max_wheel_velocity, max_wheel_velocity)
 
-    print(vLeft, vRight)
-
     # Setting Velocities
     sim.setJointTargetVelocity(motorLeft,vLeft)
     sim.setJointTargetVelocity(motorRight,vRight)
 
     sim.step()
-
-
-print("Simulation started")
